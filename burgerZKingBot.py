5c2ef12137e738eeb258dcb8ad2f1b434d79ba8c
unknown <florian.baumont@hotmail.com>
2014-10-28 14:47:00 -0400

    add function !gess

--- burgerZKingBot.py
+++ burgerZKingBot.py
@@ -1,4 +1,14 @@
 import socket
+import random
+import pymongo
+
+from pymongo import MongoClient
+from bson.objectid import ObjectId
+
+client = pymongo.MongoClient('mongodb://localhost:27017/')
+db = client['twitchBot']
+collection = db['ronnaldmcdonald']
+#posts = db.posts
 
 bot_owner = 'ronnaldmcdonald'
 botnick = 'burgerzkingbot' #Bot Name (Twitch.Tv Bot Name)
@@ -22,6 +32,26 @@ def hello(irc_sender):
 def cmd():
 	irc.send("PRIVMSG "+ channel +" :!ping / !hi /!cmd\n")
 
+def gessNumber(client, db, collection):
+  print("in gess number")
+  goal = str(18)#random.randint(1,20)
+  gess = False
+  while (gess == False):
+    irc_msg = irc.recv(1024) # receive data from the server
+    irc_sender = irc_msg.split(':')[1]
+    irc_sender = irc_sender.split('!')[0]
+
+    if irc_msg.find(goal) != -1:
+      print("TRUE")
+      #irc.send("GG")
+      irc.send("PRIVMSG "+ channel + " :Victoire !! " + irc_sender + " gagne 10 pts!\n")
+      
+      print collection.find_one({'_id': ObjectId('544fdf11795c636402b521a0')})
+      
+      collection.update({'nick':"ronnaldmcdonald"}, {'$inc': {'points' : 10}})
+      
+      gess = True
+      break
 
 
 while True:
@@ -29,11 +59,13 @@ while True:
   irc_sender = irc_msg.split(':')[1]
   irc_sender = irc_sender.split('!')[0]
   print(irc_msg) # Here we print what's coming from the server
-  print(irc_sender)
-
+  
   if irc_msg.find('!hi') != -1:
     hello(irc_sender)
   if irc_msg.find('!ping') != -1: 
     irc.send(irc_msg.replace('!ping', 'pong'))
   if irc_msg.find('!cmd') != -1:
   	cmd()
+  if irc_msg.find('!gess') != -1:
+    gessNumber(client, db, collection)
+
